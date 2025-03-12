37688fae37a990d33e64a9d27ce2e01c43b040fb
pavan-900 <pavanshankar9000@gmail.com>
2025-03-12 17:06:05 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,9 +7,9 @@ import './index.css'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  // <React.StrictMode>
     <PrimeReactProvider>
       < App />
     </PrimeReactProvider>
-  </React.StrictMode>
+  // {/* </React.StrictMode> */}
 );
